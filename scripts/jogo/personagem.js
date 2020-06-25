0cceee01b314437ac7eb5572d28ad920d7a91aa7
Nathalia Meira <nathalia.meira@hotmail.com>
2020-06-25 17:58:02 -0300

    Mais inimigos e GameOver

--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,51 +1,59 @@
 class Personagem extends Animacao {
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
- super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
-    
-    this.yInicial = height - this.altura
-    this.y = this.yInicial
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite){
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+    this.variacaoY = variacaoY;
+    this.yInicial = height - this.altura - this.variacaoY;
+    this.y = this.yInicial;
     
     this.velocidadeDoPulo = 0;
-    this.gravidade = 3;
-    
- }
- 
- pula() {
-   this.velocidadeDoPulo = -30
-}
-desce(){
+    this.gravidade = 6;
+    this.alturaDoPulo = -50
+    this.pulos = 0
+  }
+  
+  pula() {
+    if(this.pulos < 2) {
+      this.velocidadeDoPulo = this.alturaDoPulo
+      this.pulos++
+    }
+  }
+  desce(){
   this.y=this.y+ 50
 }
-volta(){
+   volta(){
   this.x=this.x-40
 }
-vai(){
+   vai(){
   this.x=this.x+50
 }
  
- 
- aplicarGravidade() {
-   this.y = this.y + this.velocidadeDoPulo
-   this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
- 
-   if(this.y > this.yInicial){
+
+
+  
+  aplicaGravidade() {
+    this.y = this.y + this.velocidadeDoPulo
+    this.velocidadeDoPulo = this.velocidadeDoPulo + this.gravidade
+    
+    if(this.y > this.yInicial){
       this.y = this.yInicial
-   }
- }
- 
- estaColidindo(inimigo) {
-   const precisao = .7
-   const colisao = collideRectRect(
-     this.x, 
-     this.y,              
-     this.largura * precisao, 
-     this.altura * precisao,
-     inimigo.x,
-     inimigo.y,
-     inimigo.largura * precisao,
-     inimigo.altura * precisao
-   
-   )
-   return colisao;
- }
+      this.pulos = 0
+    }
+  }
+  
+  estaColidindo(inimigo) {
+    const precisao = .7
+    const colisao = collideRectRect(
+      this.x, 
+      this.y, 
+      this.largura * precisao, 
+      this.altura * precisao,
+      inimigo.x,
+      inimigo.y,
+      inimigo.largura * precisao,
+      inimigo.altura * precisao
+    );
+    
+    return colisao;
+  }
+
 }
